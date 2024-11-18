3badb2a5209617a39c782d375b6a7f4f52506310
Ghostrecon123 <ternenge.dzika@binghamuni.edu.ng>
2024-11-18 09:16:17 +0100

    Add files via upload

---
+++ michael.py
@@ -0,0 +1,38 @@
+def a():
+    displacement = float(input("how far was it?"))
+    time = float(input("how long did it take?"))
+    print(displacement/time)
+
+a();
+
+def b():
+    force = float(input("how much force was used?"))
+    area = float(input("how much space was used?"))
+    print(force/area)
+
+b();
+
+def c():
+    time = float(input("how long did it take?"))
+    oscillation = float(input("how many oscillations?"))
+    print(time/oscillation)
+
+c();
+
+def d():
+    force = float(input("how much work was done"))
+    time = float(input("how long did it take"))
+    print(force/time)
+
+d();
+
+def e():
+    frequency = float(input("enter frequency?"))
+    distance = float(input("what was the distance?"))
+    print(frequency*distance)
+
+e();
+
+
+
+
