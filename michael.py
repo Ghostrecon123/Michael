317f7936c2986cdc1c7c231638144412ffc6337d
Ghostrecon123 <ternenge.dzika@binghamuni.edu.ng>
2024-11-19 22:35:58 +0100

    Update michael.py

--- michael.py
+++ michael.py
@@ -1,32 +1,32 @@
-def a():
+def a(): calculate velocity 
     displacement = float(input("how far was it?"))
     time = float(input("how long did it take?"))
     print(displacement/time)
 
 a();
 
-def b():
+def b(): calculate pressure 
     force = float(input("how much force was used?"))
     area = float(input("how much space was used?"))
     print(force/area)
 
 b();
 
-def c():
+def c(): calculate period 
     time = float(input("how long did it take?"))
     oscillation = float(input("how many oscillations?"))
     print(time/oscillation)
 
 c();
 
-def d():
+def d(): calculate power 
     force = float(input("how much work was done"))
     time = float(input("how long did it take"))
     print(force/time)
 
 d();
 
-def e():
+def e(): calculate wave velocity 
     frequency = float(input("enter frequency?"))
     distance = float(input("what was the distance?"))
     print(frequency*distance)
